35b7aca24a392af68b7cf99bbfb562f3b39d4851
navid navid <navid.otadi@gmail.com>
2025-03-08 19:13:50 +0100

    added loop on resulst

--- tunnel.py
+++ tunnel.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 import matplotlib.pyplot as plt
 
@@ -10,27 +11,31 @@ def set_to_zero(arr, tendency):
 def set_to_random(arr, tendency):
     """Set all elements of the array to random values and increase tendency."""
     tendency += 1
-    return [random.randint(0, 1000) for _ in arr], tendency
+    return [random.randint(0, sys.maxsize) for _ in arr], tendency
 
 def random_choice():
     """Choose randomly between set_to_zero and set_to_random with 50/50 probability."""
     return random.choice([set_to_zero, set_to_random])
 
-def main_run(n, iterations):
+def main_run(arr_size, iterations):
     """Run the process for a given number of iterations."""
     tendency = 0
-    arr = [0] * n  # Initialize array with n elements
+    arr = [0] * arr_size  # Initialize array with n elements
     for _ in range(iterations):
         func = random_choice()  # Choose a function
         arr, tendency = func(arr, tendency)  # Apply the function and update tendency
         #print(f"Array: {arr}, Tendency: {tendency}")  # Print the current state of the array and tendency
     return tendency
 if __name__ == "__main__":
-    tendency_array = [] 
-    for i in range(90):
-        tnd = main_run(n=50*(10*i+1), iterations=100)  # Example: using an array of size 10
-        tendency_array.append(tnd)
-    print( f' sum of tendency : {sum(tendency_array)}' )  # Print tendency for each array size
-    plt.plot(tendency_array )
-    plt.show()
+    tendey_of_tendecy =[]
+    for k in range(3):
+        tendency_array = [] 
+        for i in range(90):
+            tnd = main_run(arr_size=50*(10*i+1), iterations=100)  # iterations for each i 
+            tendency_array.append(tnd)
+       # print( f' sum of tendency : {sum(tendency_array)}' )  # Print tendency for each array size
+        tendey_of_tendecy.append( sum(tendency_array) )
 
+    print( f' sum of tendency of tendencies : {sum(tendey_of_tendecy)}' )
+    plt.plot(sum(tendey_of_tendecy) )
+    plt.show()
